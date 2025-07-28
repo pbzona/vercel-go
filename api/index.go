75cd598a2ed47c7dd46f643b9740378956fdb409
Phil Zona <phil.b.zona@gmail.com>
2025-07-28 00:45:35 -0400

    try to fix lol

--- api/index.go
+++ api/index.go
@@ -1,18 +1,36 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/pbzona/vercel-go/helpers"
 )
 
+type ApiResponse struct {
+	message string
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	helpers.HandleByMethod(helpers.HandlerMap{
 		"GET": handleGet,
 	})
 }
 
 func handleGet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%+v", r)
+	data := ApiResponse{
+		message: "Hi from Go running on Vercel",
+	}
+
+	buf := new(bytes.Buffer)
+	if err := json.NewEncoder(buf).Encode(data); err != nil {
+		http.Error(w, "Failed to encode API response", 500)
+	}
+
+	buf.WriteTo(w)
 }
